cc750c07bb79670c5a7addfdca5117149c82bd6e
jethomson <jonathan_e_thomson@yahoo.com>
2024-06-02 22:40:43 -0400

    Preserve input type text attribute in minified html.

--- minify.py
+++ minify.py
@@ -9,7 +9,7 @@ def minify(source, target, env):
   for filename in dir.iterdir():
     html = filename.read_text()
     try:
-      minified = minify_html.minify(html, minify_js=True, minify_css=True)
+      minified = minify_html.minify(html, minify_js=True, minify_css=True, keep_input_type_text_attr=True)
       with open(f'./html_minified/{filename.name}', 'w') as fout:
         print(minified, file=fout, end='')
     except SyntaxError as e:
