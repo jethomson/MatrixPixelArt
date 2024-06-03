d6876113b7c3c6abfe778265df9ff5dc1633151c
jethomson <jonathan_e_thomson@yahoo.com>
2024-06-02 22:17:54 -0400

    Add build script for minifying html.

---
+++ minify.py
@@ -0,0 +1,19 @@
+Import("env")
+env.Execute("$PYTHONEXE -m pip install minify-html")
+
+from pathlib import Path
+import minify_html
+
+def minify(source, target, env):
+  dir = Path("./html")
+  for filename in dir.iterdir():
+    html = filename.read_text()
+    try:
+      minified = minify_html.minify(html, minify_js=True, minify_css=True)
+      with open(f'./html_minified/{filename.name}', 'w') as fout:
+        print(minified, file=fout, end='')
+    except SyntaxError as e:
+      print(e)
+
+env.AddPostAction("buildfs", minify)
+
